5636863d0cd9d6285161a577d3c1af3246963e33
fabon <syobon.hinata.public@gmail.com>
2022-06-13 18:30:05 +0900

    Create project using vite and vue

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <p>日本語 English русский язык</p>
+</template>
+
+<style>
+@font-face {
+  font-family: Cyrillic;
+  src: local("Helvetica Neue"), local("Arial");
+  font-weight: normal;
+  /* only cyrillic */
+  unicode-range: U+0301, U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116, U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
+}
+@font-face {
+  font-family: Cyrillic;
+  src: local("Helvetica Neue Bold"), local("Arial Bold");
+  font-weight: bold;
+  /* only cyrillic */
+  unicode-range: U+0301, U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116, U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
+}
+#app {
+  font-family: Cyrillic, Meiryo, sans-serif;
+  color: #222;
+}
+</style>
