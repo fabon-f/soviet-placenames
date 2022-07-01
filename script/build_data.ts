f9fcdaa0d5282a698e466f81fdd5a84f42cad8b9
fabon <syobon.hinata.public@gmail.com>
2022-07-02 01:17:19 +0900

    Make build script more type-safe

--- script/build_data.ts
+++ script/build_data.ts
@@ -3,15 +3,20 @@ import { load } from 'js-yaml'
 import * as url from 'url'
 import { transliterate } from 'tensha'
 
-const citiesFile = url.fileURLToPath(new URL('../cities.yml', import.meta.url))
-const transliterationFile = url.fileURLToPath(new URL('../transliteration_ja.yml', import.meta.url))
+type OriginalCitiesData = {
+  [key: string]: {
+    [key: string]: {
+      [key: string]: {
+        nameHistory: NameHistory[]
+      }
+    }
+  }
+}
 
-const cities = load(await fs.readFile(citiesFile, { encoding: 'utf-8'}), { filename: citiesFile }) as any
-const transliterations = load(await fs.readFile(transliterationFile, 'utf-8'), { filename: transliterationFile }) as any
-
-const data = {
-  cities: [] as any[],
-  names: [] as any[]
+type TransliterationDictionary = {
+  [key: string]: {
+    [key: string]: string
+  }
 }
 
 function getJapanese(orig: string, lang: string) {
@@ -32,6 +37,42 @@ function searchLatestName(nameHistory: NameHistory[], language: string) {
   const result = nameHistory.find(name => name.period.endsWith('-') && typeof name[language] === 'string');
   if (result === undefined) { throw new Error('') }
   return result[language]
+}
+
+
+const transliterations = await (async () => {
+  const transliterationFile = url.fileURLToPath(new URL('../transliteration_ja.yml', import.meta.url))
+  const transliterationDic = load(await fs.readFile(transliterationFile, 'utf-8'), { filename: transliterationFile }) as TransliterationDictionary
+
+  // type check
+  for (const l in transliterationDic) {
+    for (const w in transliterationDic[l]) {
+      if (typeof transliterationDic[l][w] !== 'string') { throw new Error('Invalid') }
+    }
+  }
+  return transliterationDic
+}) ()
+
+const cities = await (async () => {
+  const citiesFile = url.fileURLToPath(new URL('../cities.yml', import.meta.url))
+  const cities = load(await fs.readFile(citiesFile, { encoding: 'utf-8'}), { filename: citiesFile }) as OriginalCitiesData
+
+  // type check
+  for (const a in cities) {
+    for (const b in cities[a]) {
+      for (const c in cities[a][b]) {
+        if (cities[a][b][c].nameHistory.some(n => Object.keys(n).every(k => typeof n[k] !== 'string'))) {
+          throw new Error('Invalid')
+        }
+      }
+    }
+  }
+  return cities
+}) ()
+
+const data = {
+  cities: [] as any[],
+  names: [] as any[]
 }
 
 for (const country in cities) {
