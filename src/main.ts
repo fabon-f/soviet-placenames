3d37ef1634490464370a072e900d793e2f6b0a89
fabon <syobon.hinata.public@gmail.com>
2022-07-16 10:58:37 +0900

    Preload web fonts

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+if (typeof document.fonts === 'object' && typeof document.fonts.load === 'function') {
+  // preload necessary fonts if CSS Font Loading API is supported
+  document.fonts.load('10px \'Source Sans 3\'', 'ағaş')
+}
