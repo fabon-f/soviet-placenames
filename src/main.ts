d3d3a2a167e08547d5ce1996e955ca75c275a7c4
fabon <syobon.hinata.public@gmail.com>
2022-07-19 16:11:29 +0900

    Redirect to HTTPS by JavaScript

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+if (location.protocol === 'http:' && ['lab.fabon.info', 'fabon-f.github.io'].includes(location.host)) {
+  location.replace(location.href.replace("http:", "https:"))
+}
+
 createApp(App).mount('#app')
 
 if (typeof document.fonts === 'object' && typeof document.fonts.load === 'function') {
