fa9f9a042db2d2fbf9b50d1bb42001d25474a978
fabon <syobon.hinata.public@gmail.com>
2023-12-09 04:36:05 +0900

    Refactor MapViewer with vue-leaflet

--- src/components/MapViewer.vue
+++ src/components/MapViewer.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { reactive, onMounted, onUpdated, watch, toRef } from 'vue'
-import { map as lMap, tileLayer, marker, icon, featureGroup } from 'leaflet'
-import type { Map as LMap } from 'leaflet'
+import { ref } from 'vue'
+import { LMap, LTileLayer, LFeatureGroup, LMarker, LPopup } from '@vue-leaflet/vue-leaflet'
+import { useDebounceFn } from '@vueuse/core'
+import { icon } from 'leaflet'
 
 type City = {
   id: number
@@ -10,16 +11,7 @@ type City = {
   longitude: number
 }
 
-const props = defineProps<{ cities: City[], show: boolean }>()
-
-const bound = reactive({
-  west: 0,
-  east: 0,
-  north: 0,
-  south: 0
-})
-
-const mapId = `mapviewer-${Math.floor(Math.random() * 1048576).toString(16)}`
+defineProps<{ cities: City[], show: boolean }>()
 
 const markerIcon = icon({
   iconUrl: './marker-icon.png',
@@ -32,69 +24,42 @@ const markerIcon = icon({
   shadowSize: [41, 41]
 })
 
-const createMarkersLayerGroup = (cities: City[]) => {
-  const layer = featureGroup()
-  for (const city of cities) {
-    marker([city.latitude, city.longitude], { icon: markerIcon }).addTo(layer)
-      .bindPopup(city.name)
+const mapRef = ref<InstanceType<typeof LMap> | null>(null)
+const layerRef = ref<InstanceType<typeof LFeatureGroup> | null>(null)
+
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+const attribution = '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+
+const handleMarkerLayerUpdate = useDebounceFn(() => {
+  if (!layerRef.value || !mapRef.value) {
+    return
   }
-  return layer
-}
-
-let map: LMap | null = null
-let markersLayer = createMarkersLayerGroup(props.cities)
-
-let shown = false
-
-onUpdated(() => {
-  if (!map) { return }
-  if (props.show && (!shown)) {
-    map.invalidateSize()
+  const markersLayer = layerRef.value.leafletObject
+  const map = mapRef.value.leafletObject
+  if (!markersLayer || !map) {
+    return
   }
-  if (props.show) {
-    const newBound = markersLayer.getBounds()
-    if (newBound.isValid()) {
-      map.fitBounds(newBound, { maxZoom: 7 })
-    }
+  const newBound = markersLayer.getBounds()
+  if (newBound.isValid()) {
+    map.fitBounds(newBound, { maxZoom: 7 })
   }
-  shown = props.show
-})
-
-watch(toRef(props, 'cities'), (newCities, _oldCities) => {
-  if (!map) { return }
-  map.removeLayer(markersLayer)
-  markersLayer = createMarkersLayerGroup(newCities)
-  markersLayer.addTo(map)
-})
-
-onMounted(() => {
-  const newMap = lMap(mapId).setView([58, 100], 3)
-  tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(newMap);
-
-  markersLayer.addTo(newMap)
-
-  const firstBound = newMap.getBounds()
-  bound.east = firstBound.getEast()
-  bound.west = firstBound.getWest()
-  bound.north = firstBound.getNorth()
-  bound.south = firstBound.getSouth()
-
-  newMap.on('move', _e => {
-    const newBound = newMap.getBounds()
-    bound.east = newBound.getEast()
-    bound.west = newBound.getWest()
-    bound.north = newBound.getNorth()
-    bound.south = newBound.getSouth()
-  })
-
-  map = newMap
-})
+}, 10)
 </script>
 
 <template>
-<div v-show="show" :id="mapId"></div>
+  <div v-show="show">
+    <LMap ref="mapRef" :center="[58, 100]" :zoom="3">
+      <LTileLayer :url="tileUrl" :attribution="attribution"></LTileLayer>
+      <!-- markers -->
+      <LFeatureGroup ref="layerRef" @layeradd="handleMarkerLayerUpdate" @layerremove="handleMarkerLayerUpdate">
+        <template v-for="city in cities">
+          <LMarker :latLng="[city.latitude, city.longitude]" :icon="markerIcon">
+            <LPopup>{{ city.name }}</LPopup>
+          </LMarker>
+        </template>
+      </LFeatureGroup>
+    </LMap>
+  </div>
 </template>
 
 <style>
